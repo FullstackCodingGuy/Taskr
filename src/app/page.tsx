95eece5fa9120f8149ce9922ab7f8581c52eac93
dham <fullstackcodingguy@gmail.com>
2025-02-26 13:11:38 +0530

    add task dialog added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ import {
   IconButton,
   Checkbox,
   CircularProgress,
+  DialogContent,
+  Stack,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Snackbar, { SnackbarCloseReason } from "@mui/material/Snackbar";
@@ -21,7 +23,10 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid2";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
 interface Task {
   _id: string;
@@ -31,15 +36,136 @@ interface Task {
   priority: "Low" | "Medium" | "High";
 }
 
+export interface SimpleDialogProps {
+  open: boolean;
+  selectedValue?: Task;
+  onClose: (value: string) => void;
+}
+
+function AddTaskDialog(props: SimpleDialogProps) {
+  const { onClose, selectedValue, open } = props;
+
+  const newTaskProp = {
+    title: "",
+    dueDate: new Date(),
+    priority: "Medium",
+  };
+  const [loadingState, setLoadingState] = useState(false);
+  const [newTask, setNewTask] = useState(newTaskProp);
+
+  const handleClose = () => {
+    onClose("");
+  };
+
+  const handleListItemClick = (value: string) => {
+    onClose(value);
+  };
+  const addTask = async () => {
+    if (!newTask.title || !newTask.dueDate) {
+      console.error("Validation error");
+      return;
+    }
+    setLoadingState(true);
+    const res = await axios.post<Task>("/api/tasks", newTask);
+    // setTasks([...tasks, res.data]);
+    setNewTask(newTaskProp);
+    // showSnackBar("Task added!");
+    setLoadingState(false);
+    onClose("Task added!");
+  };
+  return (
+    <Dialog onClose={handleClose} open={open} maxWidth="sm" fullWidth>
+      <DialogTitle>Add Task</DialogTitle>
+      <Divider />
+      <DialogContent>
+        <TextField
+          fullWidth
+          label="New Task"
+          variant="standard"
+          value={newTask.title}
+          onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+          // sx={{ mb: 2 }}
+        />
+
+        <Grid container spacing={2} mb={2}>
+          <Grid size={6}>
+            <TextField
+              fullWidth
+              type="date"
+              size="small"
+              value={newTask.dueDate}
+              onChange={(e) =>
+                setNewTask({ ...newTask, dueDate: e.target.value })
+              }
+              sx={{ mt: 2 }}
+            />
+          </Grid>
+          <Grid size={6}>
+            <Select
+              fullWidth
+              size="small"
+              value={newTask.priority}
+              onChange={(e) =>
+                setNewTask({
+                  ...newTask,
+                  priority: e.target.value as "Low" | "Medium" | "High",
+                })
+              }
+              sx={{ mt: 2 }}
+            >
+              <MenuItem value="Low">Low</MenuItem>
+              <MenuItem value="Medium">Medium</MenuItem>
+              <MenuItem value="High">High</MenuItem>
+            </Select>
+          </Grid>
+        </Grid>
+
+        <Button variant="contained" onClick={addTask} loading={loadingState}>
+          Add Task
+        </Button>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export function TaskHeader(props) {
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value: string) => {
+    setOpen(false);
+    props.onClose(value);
+  };
+
+  return (
+    <Stack direction="row" alignItems="center" justifyContent="space-between">
+      <Typography variant="h5" gutterBottom>
+        Tasks
+        <Badge
+          badgeContent={props?.tasks?.length}
+          color="info"
+          sx={{ ml: 2 }}
+        ></Badge>
+      </Typography>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Add Task
+      </Button>
+      <AddTaskDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+    </Stack>
+  );
+}
+
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState({
-    title: "",
-    dueDate: "",
-    priority: "Medium",
-  });
 
-  const [loadingState, setLoadingState] = useState(false);
   const [initialLoading, setInitialLoading] = useState(true); // Add initial loading state
   const [openSnackBar, setOpenSnackBar] = useState(false);
   const [snackBarMsg, setSnackBarMsg] = useState("");
@@ -54,25 +180,29 @@ export default function Home() {
     setOpenSnackBar(false);
   };
 
-  useEffect(() => {
+  const loadList = () => {
     axios.get("/api/tasks").then((res) => {
       setTasks(res.data);
       setInitialLoading(false); // Set initial loading to false after tasks are loaded
     });
+  }
+
+  useEffect(() => {
+    loadList();
   }, []);
 
-  const addTask = async () => {
-    if (!newTask.title || !newTask.dueDate) {
-      console.error("Validation error");
-      return;
-    }
-    setLoadingState(true);
-    const res = await axios.post<Task>("/api/tasks", newTask);
-    setTasks([...tasks, res.data]);
-    setNewTask({ title: "", dueDate: "", priority: "Medium" });
-    showSnackBar("Task added!");
-    setLoadingState(false);
-  };
+  // const addTask = async () => {
+  //   if (!newTask.title || !newTask.dueDate) {
+  //     console.error("Validation error");
+  //     return;
+  //   }
+  //   setLoadingState(true);
+  //   const res = await axios.post<Task>("/api/tasks", newTask);
+  //   setTasks([...tasks, res.data]);
+  //   setNewTask({ title: "", dueDate: "", priority: "Medium" });
+  //   showSnackBar("Task added!");
+  //   setLoadingState(false);
+  // };
 
   const toggleTask = async (task: Task) => {
     const res = await axios.put<Task>(`/api/task?id=${task._id}`, {
@@ -100,70 +230,20 @@ export default function Home() {
     hideSnackBar();
   };
 
+  const onDialogClose = (msg: string) => {
+    showSnackBar(msg);
+    loadList();
+  };
   return (
     <Container maxWidth="md" sx={{ mt: 1 }}>
       <Paper elevation={3} sx={{ p: 4 }}>
-        <Typography variant="h5" gutterBottom>
-          Tasks
-          <Badge
-            badgeContent={tasks?.length}
-            color="info"
-            sx={{ ml: 2 }}
-          ></Badge>
-        </Typography>
-        <TextField
-          fullWidth
-          label="New Task"
-          variant="standard"
-          value={newTask.title}
-          onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-          // sx={{ mb: 2 }}
-        />
-        <Box sx={{ flexGrow: 1, mb: 2 }}>
-          <Grid container spacing={2}>
-            <Grid size={3}>
-              <TextField
-                fullWidth
-                type="date"
-                size="small"
-                value={newTask.dueDate}
-                onChange={(e) =>
-                  setNewTask({ ...newTask, dueDate: e.target.value })
-                }
-                sx={{ mt: 2 }}
-              />
-            </Grid>
-            <Grid size={3}>
-              <Select
-                fullWidth
-                size="small"
-                value={newTask.priority}
-                onChange={(e) =>
-                  setNewTask({
-                    ...newTask,
-                    priority: e.target.value as "Low" | "Medium" | "High",
-                  })
-                }
-                sx={{ mt: 2 }}
-              >
-                <MenuItem value="Low">Low</MenuItem>
-                <MenuItem value="Medium">Medium</MenuItem>
-                <MenuItem value="High">High</MenuItem>
-              </Select>
-            </Grid>
-          </Grid>
-        </Box>
-
-        <Button variant="contained" onClick={addTask} loading={loadingState}>
-          Add Task
-        </Button>
-        {/* <Divider sx={{mt:2}}/> */}
+        <TaskHeader {...{ tasks, onClose: onDialogClose }} />
 
         {initialLoading ? ( // Show loading icon if initial loading is true
           <Box display="flex" justifyContent="center" sx={{ mt: 2 }}>
             <CircularProgress />
-          </Box>  
-      ) : (
+          </Box>
+        ) : (
           <List sx={{ mt: 1, mx: -3 }}>
             {tasks.map((task) => (
               <ListItem
@@ -179,7 +259,7 @@ export default function Home() {
                   onChange={() => toggleTask(task)}
                 />
                 <ListItemText
-                  primary={`${task.title} (Due: ${task.dueDate})`}
+                  primary={`${task.title} (Due: ${format(task.dueDate ?? new Date(), "dd MMM")})`}
                   secondary={`Priority: ${task.priority}`}
                   sx={{
                     textDecoration: task.completed ? "line-through" : "none",
